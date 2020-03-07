c9d1a171267b11fd33d9b0a1a97a2efb0a1fd228
Ricardo José Gómez <rjgdevapp@gmail.com>
2020-03-07 10:20:45 -0500

    add oauth components and config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from "react"
+import { Router, Route, Switch } from "react-router-dom"
+import NavBar from "./components/NavBar"
+import { useAuth0 } from "./react-auth0-spa"
+import history from "./utils/history"
+import Profile from "./components/Profile"
+import PrivateRoute from "./components/PrivateRoute"
 
-function App() {
-	return (
-		<div className="App">
-		</div>
-	)
+const App = () => {
+
+	const { loading } = useAuth0()
+
+  	if (loading) {
+    	return <div>Loading...</div>
+  	}
+
+  	return (
+    	<div className="App">
+      		<Router history={history}>
+        		<header>
+          			<NavBar />
+        		</header>
+        		<Switch>
+          			<Route path="/" exact />
+          			<PrivateRoute path="/profile" component={Profile} />
+        		</Switch>
+      		</Router>
+    	</div>
+  	)
 }
 
 export default App
